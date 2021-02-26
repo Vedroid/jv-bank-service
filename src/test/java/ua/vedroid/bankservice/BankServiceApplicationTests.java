4a646dbf90a090a364d5fcf1e72a711f970933f6
Vadim <Vedroidua@gmail.com>
2021-02-26 18:03:54 +0200

    Initial commit

---
+++ src/test/java/ua/vedroid/bankservice/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vedroid.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
