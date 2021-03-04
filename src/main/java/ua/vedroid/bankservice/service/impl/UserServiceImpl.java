148c690421a75ce008b563f9659ea35da52b09e1
Vadim <Vedroidua@gmail.com>
2021-03-04 16:20:46 +0200

    Added password encoding to UserService

--- src/main/java/ua/vedroid/bankservice/service/impl/UserServiceImpl.java
+++ src/main/java/ua/vedroid/bankservice/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ua.vedroid.bankservice.service.impl;
 
 import java.util.Set;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ua.vedroid.bankservice.entity.User;
 import ua.vedroid.bankservice.exception.NoEntityException;
@@ -12,18 +13,21 @@ import ua.vedroid.bankservice.service.UserService;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final BCryptPasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final RoleService roleService;
 
     @Override
     public User create(User user) {
         user.setRoles(Set.of(roleService.getByName("USER")));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     @Override
     public User update(User user) {
         user.setRoles(findById(user.getId()).getRoles());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
